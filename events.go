c38220e8524d21d814cceddaf19c8269610d5182
raulincze <raulincze@gmail.com>
2020-03-19 16:35:50 +0000

    Calling global listeners even when there are no normal listneres.

--- events.go
+++ events.go
@@ -131,6 +131,10 @@ type eventObserver struct {
 }
 
 func (eo *eventObserver) Change(data []byte) error {
+	if data == nil {
+		return nil
+	}
+
 	e := &Event{}
 
 	if eo.typer != nil {
@@ -144,21 +148,17 @@ func (eo *eventObserver) Change(data []byte) error {
 	eo.lock.RLock()
 	defer eo.lock.RUnlock()
 
-	if eo.listeners == nil {
-		return nil
+	if eo.listeners != nil {
+		for _, listener := range eo.listeners[e.ID] {
+			//If the listeners do a lot of work ,kicking these off in goroutines might be worth
+			listener(e)
+		}
 	}
 
-	for _, listener := range eo.listeners[e.ID] {
-		//If the listeners do a lot of work ,kicking these off in goroutines might be worth
-		listener(e)
-	}
-
-	if eo.globalListeners == nil {
-		return nil
-	}
-
-	for _, globalListener := range eo.globalListeners {
-		globalListener(e)
+	if eo.globalListeners != nil {
+		for _, globalListener := range eo.globalListeners {
+			globalListener(e)
+		}
 	}
 
 	return nil
